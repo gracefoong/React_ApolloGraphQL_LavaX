14f0561c03bbbbf52e52ee6c7ec335d66edc0789
gracefoong <77376811+gracefoong@users.noreply.github.com>
2021-05-17 17:19:35 +0800

    Add files via upload

--- server/config/Schemas/TypeDefs/JobType.js
+++ server/config/Schemas/TypeDefs/JobType.js
@@ -1,1 +1,23 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLInt, GraphQLString } = graphql;
 
+const JobType = new GraphQLObjectType({
+  name: "Job",
+  fields: () => ({
+    company: { type: GraphQLString },
+    education: { type: GraphQLString },
+    experience: { type: GraphQLString },
+    industry: { type: GraphQLString },
+    jobdescription: { type: GraphQLString },
+    jobid: { type: GraphQLString },
+    jobtitle: { type: GraphQLString },
+    numberofpositions: { type: GraphQLString },
+    payrate: { type: GraphQLString },
+    postdate: { type: GraphQLString },
+    site_name: { type: GraphQLString },
+    skills: { type: GraphQLString },
+    uniq_id: { type: GraphQLString },
+  }),
+});
+
+module.exports = JobType;
