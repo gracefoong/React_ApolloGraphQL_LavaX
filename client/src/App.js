ae852cedc3d7c5dd0618c1dc66e17411560e4349
gracefoong <77376811+gracefoong@users.noreply.github.com>
2021-05-17 17:15:14 +0800

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
+import GetJobs from "./Components/GetJobs";
+/* import Form from "./Components/Form"; */
+
+
+const errorLink = onError(({ graphqlErrors, networkError }) => {
+  if (graphqlErrors) {
+    graphqlErrors.map(({ message, location, path }) => {
+      alert(`Graphql error ${message}`);
+    });
+  }
+});
+
+const link = from([
+  errorLink,
+  new HttpLink({ uri: "http://localhost:6969/graphql" }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      {" "}
+      <GetJobs />
+      {/* <Form /> */}
+    </ApolloProvider>
+  );
+}
+
+export default App;
